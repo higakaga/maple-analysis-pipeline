e6e6b5f2794d9ac7bd33e7ecaf64da963e7d4f76
higakaga <higakaga@gmail.com>
2025-09-13 21:25:11 +0900

    chore: modify deploy.sh to reflect vars

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,13 @@ set -e
 APP_DIR="/home/ubuntu/airflow"
 cd $APP_DIR
 
-# .env 파일 생성
-cat > .env <<EOF
-AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=postgresql+psycopg2://${RDS_USER}:${RDS_PASSWORD}@${RDS_HOST}:5432/${RDS_META}
-AIRFLOW__CORE__EXECUTOR=LocalExecutor
-AIRFLOW__CORE__LOAD_EXAMPLES=False
-EOF
-
+# 폴더 권한 airflow로
 mkdir -p ./dags ./logs ./plugins
 sudo chown -R 50000:50000 ./dags ./logs ./plugins
 
 # DB 초기화 (최초 실행 시만 적용)
 (docker compose run --rm airflow-init || true) 2>/dev/null || true
 
+# 컨테이너 재시작
 docker compose down -v || true
 docker compose up -d --build
